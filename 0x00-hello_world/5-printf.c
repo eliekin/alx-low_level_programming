1e91cf5e136ee00164d56138698602aca5bac401
eliekin <kineliekinahwa@gmail.com>
2022-09-08 15:06:42 +0000

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,12 @@
 
 /**
 *using printf
+*returns 0
+*main function is integer, receiving no input
 */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
