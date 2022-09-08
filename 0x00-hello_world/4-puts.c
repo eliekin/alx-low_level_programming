b5cf408825763180f72f7a274f63d8eaccf48134
eliekin <kineliekinahwa@gmail.com>
2022-09-08 14:45:26 +0000

    prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-/**
-*
-*prints using puts
-*/
+/*prints using puts*/
 
 int main(void)
 {   
     char motto[] = "\"Programming is like building a multilingual puzzle";
 
-    puts(motto);
-    return (0);
+     puts(motto);
+      return (0);
 
 }
