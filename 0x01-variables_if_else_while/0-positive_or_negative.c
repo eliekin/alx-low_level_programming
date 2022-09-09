f47afbeb4c3939e326efe2e095ef24b948459df4
eliekin <kineliekinahwa@gmail.com>
2022-09-09 10:32:36 +0000

    checks if a number is positif or negatif

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point of the program 
+*
+* Description: This program will assign a random number to the variable n each time it is executed. 
+* Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+* 
+* return: always returns zero 
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is positif\n", n);
+	}
+	return (0);
+}
