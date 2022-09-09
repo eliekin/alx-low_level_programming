8b469368c1c6dbdaae1960426fd3d057a71fda0c
eliekin <kineliekinahwa@gmail.com>
2022-09-09 21:17:16 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main: entry point of the program 
+*
+* Description: prints the alphabet
+*
+* Return: always zero
+*/
+
+int main(void)
+{
+	char c;
+
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(tolower(c));
+	return (0);
+}
