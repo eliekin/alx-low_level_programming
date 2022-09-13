dc65a5812a811063f0fd28d7f92945c6c3f80e80
eliekin <kineliekinahwa@gmail.com>
2022-09-13 20:48:53 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <ctype.c>
 /**
-* main
+* _isalpha - 
 * 
-* Description:
+* Description: checks for alphabetic character
 *
-* Return
+* Return: 1 or 0 depending on the parameter value
 */
 int _isalpha(int c)
 {
-	int response = isalpha(c);
+	int response;
+
+	response = isalpha(c);
 	if (response > 0)
 	{
 		return (1);
