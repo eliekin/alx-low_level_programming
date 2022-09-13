427f0de58c9280487bd2a679b6d0d72e88d4397a
eliekin <kineliekinahwa@gmail.com>
2022-09-13 22:20:55 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,14 @@
 void print_alphabet_x10(void)
 {
 	int times = 0;
-	char c = 'a';
 
 	while (times < 10)
-	{
-		c = 'a';
-		while (c <= 'z')
+	{	
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			c++;
 		}
 		_putchar('\n');
 
