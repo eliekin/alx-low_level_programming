8286ab45c5008405adfa2c35c108c7df36bda832
eliekin <kineliekinahwa@gmail.com>
2022-09-13 19:48:04 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* main
+* 
+* Description
+*
+* Return
+*/
+int print_sign(int c)
+{
+	if(c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
