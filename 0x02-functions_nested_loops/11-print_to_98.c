57d3262cdc8b96b478dd23305541a16f17dc84ee
eliekin <kineliekinahwa@gmail.com>
2022-09-13 22:50:44 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints integer to 98
+ * @n: n is an integer
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		printf(n);
+		printf(', ');
+		n++;
+	}
+}
