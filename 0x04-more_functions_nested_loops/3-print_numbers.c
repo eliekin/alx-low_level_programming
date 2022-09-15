07293466ada305a46157f46f384a28ef5e0174b9
eliekin <kineliekinahwa@gmail.com>
2022-09-15 11:48:16 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers- prints numbers
+* 
+* Return : always zero
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+} 
