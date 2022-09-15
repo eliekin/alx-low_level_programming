b5a147e367b7ce338e9b4d20eda5e8d2bd8146a6
eliekinahwa <86865836+eliekin@users.noreply.github.com>
2022-09-15 09:36:25 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -19,7 +19,14 @@ int main(void)
 	while (times <= 49)
 	{
 		nextNum = num1 + num2;
-		printf("%li, ", nextNum);
+		if (times == 49)
+		{
+			printf("%li", nextNum);
+		}
+		else
+		{
+			printf("%li, ", nextNum);
+		}
 		num1 = num2;
 		num2 = nextNum;
 		times++;
