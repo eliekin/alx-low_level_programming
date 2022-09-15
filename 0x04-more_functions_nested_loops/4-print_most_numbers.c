c0aef47dd48032a20784922bed85765d3bee831f
eliekin <kineliekinahwa@gmail.com>
2022-09-15 12:52:02 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 48; num <= 57; num++)
 	{
-		if (num != 2 || num != 4)
+		if (num != 50 || num != 52)
 		{
 			_putchar(num);
 		}
