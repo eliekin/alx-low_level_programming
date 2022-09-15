b5dae670e2f6452de80d36dbc931356325993f40
eliekin <kineliekinahwa@gmail.com>
2022-09-15 12:20:51 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_most_numbers - prints most numbers
+*
+* Description: prints most numbers
+*
+* Return: returns nothing
+*/
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 || num != 4)
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
