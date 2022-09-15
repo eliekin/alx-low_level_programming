c81d84ca997e497817a4613a8395228ac20372ac
eliekin <kineliekinahwa@gmail.com>
2022-09-15 12:32:18 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* more_numbers - prints numbers
+*
+* Description: prints numbers
+*
+* Return: returns nothing
+*/
+
+void more_numbers(void)
+{
+	int times;
+	int num;
+
+	times = 0;
+	num = 0;
+	while (times < 9)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num);
+		}
+		times++;
+	}
+}
