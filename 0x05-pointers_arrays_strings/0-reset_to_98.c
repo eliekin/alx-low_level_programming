74b2513645e35f28221de8518af614b55c34d2f4
eliekinahwa <86865836+eliekin@users.noreply.github.com>
2022-09-19 15:24:25 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* rest_to_98 - updates the value it points to to 98
-* @*n: parameter
+* reset_to_98 - updates the value it points to to 98
+* @n: parameter
 *
 * Return: returns nothing
 */
