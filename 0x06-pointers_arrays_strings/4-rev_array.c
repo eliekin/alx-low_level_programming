d45f3785a38c9f21813391c1981824e6ae23a7c7
eliekin <kineliekinahwa@gmail.com>
2022-09-21 15:27:40 +0000

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,13 +6,18 @@
 * @n: parameter
 * Return: void
 */
-
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int half = n / 2;
+	int left, right, i;
 
-	for (j = n - 1; j >= 0; j--)
+	n -= 1;
+
+	for (i = 0; i < half; i++)
 	{
-		a[j];
+		left = a[n - i];
+		right = a[i];
+		a[i] = left;
+		a[n - i] = right;
 	}
 }
