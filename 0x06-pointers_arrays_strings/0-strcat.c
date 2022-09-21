7d48a00159929264c9fe4990c046080236e4a57d
eliekin <kineliekinahwa@gmail.com>
2022-09-21 15:23:17 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,23 @@
 #include "main.h"
-#inlcude <string.h>
+
 /**
 * _strcat - concatenates strings
 * @dest: parameter
 * @char: parameter
 * Return: returns a character
 */
-char *_strcat(char *dest, char *strc)
+char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, strc));
+	int i;
+	int n = 0;
+
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[n + i] = src[i];
+	dest[n + i] = '\0';
+
+	return (dest);
 }
