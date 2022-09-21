a5bde10185a658aaf1fc9dab3351bf2f480419ba
eliekin <kineliekinahwa@gmail.com>
2022-09-21 14:25:38 +0000

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp - compares two strings
+* @s1: parameter
+* @s2: parameter
+* Return: returns integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = strcmp(s1, s2);
+	return (i);
+}
