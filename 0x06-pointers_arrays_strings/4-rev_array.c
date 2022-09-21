92ea69fd499a7e55424cb396d833cd4729f3df0b
eliekin <kineliekinahwa@gmail.com>
2022-09-21 14:37:37 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses an array
+* @a: parameter
+* @n: parameter
+* Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (j = n - 1; j >= 0; j--)
+	{
+		a[j];
+	}
+}
