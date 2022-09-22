b9db0af40290ac29c5419def4f1a22234131fa60
eliekin <kineliekinahwa@gmail.com>
2022-09-22 10:57:29 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+* rot13 - encodes using rot13
+* @s: parameter
+* Return: character
+*/
+char *rot13(char *s)
+{
+	int n, length, key;
+
+	key = 13;
+	length = strlen(s);
+	for (n = 0, n < length; n++)
+	{
+		int currentLetter;
+		char cipher;
+
+		currentLetter = s[n];
+		cipher = currentLetter + key;
+		if ((currentLetter - 'a') + key > 26)
+		{
+			key = (currentLetter - 'a') + key) % 26;
+			cipher = 'a' + key;
+		}
+		key = 13;
+	}
+	return (s[cipher]);
+}
