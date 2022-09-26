9f8237890304670eda1a49016eeacbb0455ff286
eliekin <kineliekinahwa@gmail.com>
2022-09-26 11:30:23 +0000

    main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+int _putchar(char c);
+char *_strstr(char *haystack, char *needle);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
