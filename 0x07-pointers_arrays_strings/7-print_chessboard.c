4328a277c884864e553a30a42cbde9325a9673e2
eliekin <kineliekinahwa@gmail.com>
2022-09-26 15:19:54 +0000

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_chassboard - prints the chessboard
+* @a: parameter
+*
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	
+	i = 0;
+	j = 0;
+	for (a[i]; i < 9; i++)
+	{
+		for (a[j]; j < 9; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	}
+}
