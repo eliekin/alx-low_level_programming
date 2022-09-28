43fc3de9c754eac441249a3f3b16443fd4459abd
eliekin <kineliekinahwa@gmail.com>
2022-09-28 14:12:38 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+/**
+* _sqrt_recursion - returns the natural square root
+* @n: parameter
+*
+* Return: int
+*/
+int _sqrt_recursion(int n)
+{
+	int e;
+
+	e = sqrt(n);
+	return (e);
+}
