b15053e566ce67bce35e89b37cfe7f6ecdcc7e0d
eliekin <kineliekinahwa@gmail.com>
2022-09-28 11:11:50 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen_recursion - returns the length of s
+* @s: parameter
+*
+* Return: int
+*/
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	return (i);
+}
