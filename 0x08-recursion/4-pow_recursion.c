01ad542a3fc84c43d686b69c2d45b48e72a425c4
eliekin <kineliekinahwa@gmail.com>
2022-09-28 13:53:55 +0000

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,7 +13,14 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 	{
 		return (-1);
-	}	
-	x = x * _pow_recursion(x, y - 1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y >= 0)
+	{
+		x = x * _pow_recursion(x, y - 1);
+	}
 	return (x);
 }
