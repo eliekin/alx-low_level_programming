9a1d880b577a42915459656e8b893e152ff4e68e
eliekinahwa <86865836+eliekin@users.noreply.github.com>
2022-09-28 12:29:46 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,15 +9,15 @@ int factorial(int n)
 {
 	if (n == 0)
 	{
-		return 1;
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else
 	{
 		n = n * factorial(n - 1);
-		return n;
+		return (n);
 	}
 }
