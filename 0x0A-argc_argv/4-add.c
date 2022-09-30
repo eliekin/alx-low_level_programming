e58045fca295a45c6d12e45a7e179d53b49e283f
eliekin <kineliekinahwa@gmail.com>
2022-09-30 15:21:38 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - entry point
+* @argc: parameter
+* @argv: parameter
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	int result, i;
+
+	result = 0;
+	if (argc == 0)
+	{
+		printf("%d\n", argc);
+	}
+	for (i = 1; i <= argc; i++)
+	{
+		if (argv[i] >= 'a' || argv[i] <= 'z')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			result += atoi(argv[i]);
+		}
+	}
+	return (0);
+}
