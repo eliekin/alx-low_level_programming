fe0972ef11743aa4f5f768c92df54220db3a0521
eliekinahwa <86865836+eliekin@users.noreply.github.com>
2022-09-30 14:59:53 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
 * @argv: parameter
 * Return: int
 */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
