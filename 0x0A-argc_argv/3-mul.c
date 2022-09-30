9f9da8968f1814e0f49cc88ae79ed8f4a1d2a6d9
eliekinahwa <86865836+eliekin@users.noreply.github.com>
2022-09-30 16:00:37 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 	long int result;
 	int args = argc - 1;
 
-	if (args == 0 || args >= 3)
+	if (args <= 2 || args > 3)
 	{
 		printf("Error");
 		printf("\n");
 		return (1);
 	}
-	if (args > 1)
+	else
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d", result);
