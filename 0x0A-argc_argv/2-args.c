b4d39933e445b48e911137e7ec02b9dc852413fe
eliekin <kineliekinahwa@gmail.com>
2022-09-30 14:18:15 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: parameter
+* @argv: parameter
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
+}
